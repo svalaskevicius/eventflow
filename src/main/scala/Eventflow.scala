d39f0e808f741c38b79fe673779b6fa27fbe6bb0
Sarunas Valaskevicius <rakatan@gmail.com>
2015-10-06 21:55:32 +0100

    rename variables for clarity

--- src/main/scala/Eventflow.scala
+++ src/main/scala/Eventflow.scala
@@ -5,7 +5,7 @@ import Cqrs.InMemoryDb._
 object Eventflow {
 
   def main(args: Array[String]) {
-    val result = for {
+    val requests = for {
       c <- newCounter("test counter")
       _ <- c.handleCommand(Increment)
       _ <- c.handleCommand(Increment)
@@ -21,8 +21,8 @@ object Eventflow {
       _ <- c.handleCommand(Increment)
     } yield (())
     println("------------")
-    val result1 = runCounter(result)
-    runInMemoryDb(newDb)(result1) fold(err => println("Error occurred: " + err), _ => println("OK"))
+    val program = runCounter(requests)
+    runInMemoryDb(newDb)(program) fold(err => println("Error occurred: " + err), _ => println("OK"))
     println("------------")
   }
 }
