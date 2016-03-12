fce8a4c5a26418319ccc3e6eee69825de0e4dd83
Sarunas Valaskevicius <rakatan@gmail.com>
2016-03-12 12:22:27 +0000

    enable door tests again

--- src/test/scala/DoorSpec.scala
+++ src/test/scala/DoorSpec.scala
@@ -9,78 +9,71 @@ import scala.collection.immutable.TreeMap
 
 class DoorSpec extends FlatSpec with Matchers with AggregateSpec {
 
-//  "Door" should "be open at start" in {
-//    given {
-//      newDbRunner
-//        .withEvent(tag, "door", Registered("door"))
-//    } when {
-//      _.command(DoorAggregate, "door", Close)
-//    } thenCheck {
-//      _.newEvents[Event](tag, "door") should be(List(Closed))
-//    }
-//  }
-//
-//  "Closing door" should "allow open it again" in {
-//    given {
-//      newDbRunner
-//        .withEvents[Event](tag, "door", Registered("door"), Closed)
-//    } when {
-//      _.command(DoorAggregate, "door", Open)
-//    } thenCheck {
-//      _.newEvents[Event](tag, "door") should be(List(Opened))
-//    }
-//  }
-//
-//  it should "allow locking door" in {
-//    given {
-//      newDbRunner
-//        .withEvents[Event](tag, "door", Registered("door"), Closed)
-//    } when {
-//      _.command(DoorAggregate, "door", Lock("key"))
-//    } thenCheck {
-//      _.newEvents[Event](tag, "door") should be(List(Locked("key")))
-//    }
-//  }
-//
-//  "Locked door" should "allow to unclock with the same key" in {
-//    given {
-//      newDbRunner
-//        .withEvents[Event](tag, "door", Registered("door"), Closed, Locked("key"))
-//    } when {
-//      _.command(DoorAggregate, "door", Unlock("key"))
-//    } thenCheck {
-//      _.newEvents[Event](tag, "door") should be(List(Unlocked("key")))
-//    }
-//  }
-//
-//  it should "fail to unlock with the different key" in {
-//    given {
-//      newDbRunner
-//        .withEvents[Event](tag, "door", Registered("door"), Closed, Locked("key"))
-//    } check {
-//      _.failedCommandError(DoorAggregate, "door", Unlock("wrongkey")) should be(Errors(NEL(ErrorCommandFailure("Attempted unlock key is invalid"))))
-//    }
-//  }
-//
-//  it should "not allow to open the door" in {
-//    given {
-//      newDbRunner
-//        .withEvents[Event](tag, "door", Registered("door"), Closed, Locked("key"))
-//    } check {
-//      _.failedCommandError(DoorAggregate, "door", Open) should be(Errors(NEL(ErrorCommandFailure("Locked door can only be unlocked."))))
-//    }
-//  }
-//
-//  "Unlocked doors" should "allow to be opened" in {
-//    given {
-//      newDbRunner
-//        .withEvents[Event](tag, "door", Registered("door"), Closed, Locked("key"), Unlocked("key"))
-//    } when {
-//      _.command(DoorAggregate, "door", Open)
-//    } thenCheck {
-//      _.newEvents[Event](tag, "door") should be(List(Opened))
-//    }
-//  }
+  "Door" should "be open at start" in {
+    given {
+      newDb.withEvent(tag, "door", Registered("door"))
+    } when {
+      _.command(DoorAggregate, "door", Close)
+    } thenCheck {
+      _.newEvents[Event](tag, "door") should be(List(Closed))
+    }
+  }
+
+  "Closing door" should "allow open it again" in {
+    given {
+      newDb.withEvents[Event](tag, "door", Registered("door"), Closed)
+    } when {
+      _.command(DoorAggregate, "door", Open)
+    } thenCheck {
+      _.newEvents[Event](tag, "door") should be(List(Opened))
+    }
+  }
+
+  it should "allow locking door" in {
+    given {
+      newDb.withEvents[Event](tag, "door", Registered("door"), Closed)
+    } when {
+      _.command(DoorAggregate, "door", Lock("key"))
+    } thenCheck {
+      _.newEvents[Event](tag, "door") should be(List(Locked("key")))
+    }
+  }
+
+  "Locked door" should "allow to unclock with the same key" in {
+    given {
+      newDb.withEvents[Event](tag, "door", Registered("door"), Closed, Locked("key"))
+    } when {
+      _.command(DoorAggregate, "door", Unlock("key"))
+    } thenCheck {
+      _.newEvents[Event](tag, "door") should be(List(Unlocked("key")))
+    }
+  }
+
+  it should "fail to unlock with the different key" in {
+    given {
+      newDb.withEvents[Event](tag, "door", Registered("door"), Closed, Locked("key"))
+    } check {
+      _.failedCommandError(DoorAggregate, "door", Unlock("wrongkey")) should be(Errors(NEL(ErrorCommandFailure("Attempted unlock key is invalid"))))
+    }
+  }
+
+  it should "not allow to open the door" in {
+    given {
+      newDb.withEvents[Event](tag, "door", Registered("door"), Closed, Locked("key"))
+    } check {
+      _.failedCommandError(DoorAggregate, "door", Open) should be(Errors(NEL(ErrorCommandFailure("Locked door can only be unlocked."))))
+    }
+  }
+
+  "Unlocked doors" should "allow to be opened" in {
+    given {
+      newDb.withEvents[Event](tag, "door", Registered("door"), Closed, Locked("key"), Unlocked("key"))
+    } when {
+      _.command(DoorAggregate, "door", Open)
+    } thenCheck {
+      _.newEvents[Event](tag, "door") should be(List(Opened))
+    }
+  }
 //
 //  "Door projection" should "return the current state" in {
 //    import Domain.Door
