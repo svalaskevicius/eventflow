8ee883f295c6093115c4c7e7eb1cf056cf67c6e9
Sarunas Valaskevicius <rakatan@gmail.com>
2016-04-04 09:28:46 +0100

    move behaviour closer to its usage

--- example/src/main/scala/Domain/Returns.scala
+++ example/src/main/scala/Domain/Returns.scala
@@ -18,20 +18,6 @@ object Store {
   type Timestamp = Long
   type Money = Int
 
-  final case class StoreInfo(inventory: Map[ProductId, Int], prices: Map[ProductId, Money], knownReceipts: Map[SequenceNumber, Receipt]) {
-
-    def returnProduct(receipt: Receipt) = copy(knownReceipts = knownReceipts - receipt.sequenceNumber)
-
-    def add(productId: ProductId, qty: Int) = copy(inventory = inventory.updated(productId, quantity(productId) + qty))
-
-    def quantity(productId: ProductId) = inventory.getOrElse(productId, 0)
-
-    def addReceipt(receipt: Receipt) = copy(knownReceipts = knownReceipts.updated(receipt.sequenceNumber, receipt))
-  }
-  object StoreInfo {
-    def empty = StoreInfo(TreeMap[ProductId, Int](), TreeMap[ProductId, Money](), TreeMap[SequenceNumber, Receipt]())
-  }
-
   final case class Receipt(sequenceNumber: SequenceNumber, product: ProductId, quantity: Int, amount: Money, timestamp: Timestamp)
 
   sealed trait RefundType
@@ -48,24 +34,29 @@ object Store {
   final case class ItemRestocked(id: AggregateId, timestamp: Timestamp, productId: ProductId, qty: Int) extends Event
 
   sealed trait Command
-  final case class RequestRefund(id: AggregateId, timestamp: Timestamp, receipt: Receipt, refundType: RefundType, productState: ProductState) extends Command {
-    def isNotExpiredForCash = isBefore(timestamp, receipt.timestamp, 30, DAYS) || (refundType == StoreCredit)
-    def isNotExpired = isBefore(timestamp, receipt.timestamp, 12, MONTHS)
-  }
-
-  private def isBefore(timestamp: Timestamp, base: Timestamp, delta: Long, unit: ChronoUnit) = {
-    val time = LocalDateTime.ofEpochSecond(timestamp, 0, UTC)
-    val b = LocalDateTime.ofEpochSecond(base, 0, UTC).plus(delta, unit)
-    time.isBefore(b)
-  }
+  final case class RequestRefund(id: AggregateId, timestamp: Timestamp, receipt: Receipt, refundType: RefundType, productState: ProductState) extends Command
 }
 
 object StoreAggregate extends EventFlow[Event, Command] {
 
+  final case class StoreInfo(inventory: Map[ProductId, Int], prices: Map[ProductId, Money], knownReceipts: Map[SequenceNumber, Receipt]) {
+
+    def returnProduct(receipt: Receipt) = copy(knownReceipts = knownReceipts - receipt.sequenceNumber)
+
+    def add(productId: ProductId, qty: Int) = copy(inventory = inventory.updated(productId, quantity(productId) + qty))
+
+    def quantity(productId: ProductId) = inventory.getOrElse(productId, 0)
+
+    def addReceipt(receipt: Receipt) = copy(knownReceipts = knownReceipts.updated(receipt.sequenceNumber, receipt))
+  }
+  object StoreInfo {
+    def empty = StoreInfo(TreeMap[ProductId, Int](), TreeMap[ProductId, Money](), TreeMap[SequenceNumber, Receipt]())
+  }
+
   val store: RegisteredFlowStateAux[StoreInfo] = ref('store, storeInfo => handler(
     when[RequestRefund].
-      guard(_.isNotExpiredForCash, "The receipt has expired for cash refunds.").
-      guard(_.isNotExpired, "The receipt has expired for refunds.").
+      guard(isNotExpiredForCash, "The receipt has expired for cash refunds.").
+      guard(isNotExpired, "The receipt has expired for refunds.").
       guard(cmd => storeInfo.knownReceipts.contains(cmd.receipt.sequenceNumber), "Unkown receipt number.").
       emitEvents { cmd =>
         val stockEvents = cmd.productState match {
@@ -85,5 +76,15 @@ object StoreAggregate extends EventFlow[Event, Command] {
   val snapshottableStates: FlowStates = List(store)
 
   val aggregateLogic = store.state(StoreInfo.empty)
+
+  private def isNotExpiredForCash(r: RequestRefund) = isBefore(r.timestamp, r.receipt.timestamp, 30, DAYS) || (r.refundType != CashRefund)
+
+  private def isNotExpired(r: RequestRefund) = isBefore(r.timestamp, r.receipt.timestamp, 12, MONTHS)
+
+  private def isBefore(timestamp: Timestamp, base: Timestamp, delta: Long, unit: ChronoUnit) = {
+    val time = LocalDateTime.ofEpochSecond(timestamp, 0, UTC)
+    val b = LocalDateTime.ofEpochSecond(base, 0, UTC).plus(delta, unit)
+    time.isBefore(b)
+  }
 }
 
